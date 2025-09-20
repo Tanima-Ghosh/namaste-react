270d644d6947ca78d1b9170842e4206f684e6239
Tanima Ghosh <contact.tanimaghosh@gmail.com>
2025-09-20 05:53:34 -0700

    add new package file

--- App.js
+++ App.js
@@ -1,16 +1,4 @@
-/*<div id="parent">
-    <div id="child">
-        <h1>I am an h1 element</h1>
-        <h2>I am an h1 element</h2>
-    </div>
 
-     <div id="child2">
-        <h1>I am an h1 element</h1>
-        <h2>I am an h1 element</h2>
-    </div>
-</div>*/
-
-// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
