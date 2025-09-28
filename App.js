2d7fb9e679a6733fd992aa5a50149d48ba50e56c
Tanima Ghosh <contact.tanimaghosh@gmail.com>
2025-09-28 00:54:50 -0700

    ingnite our app - component

--- App.js
+++ App.js
@@ -3,18 +3,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is an h1 element"),
-    React.createElement("h2", {}, "I am an h2 element"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 element"),
-    React.createElement("h2", {}, "I am an h2 element"),
-  ]),
-]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// react
 
-console.log(parent);
+const heading = React.createElement("h1", {id: "heading"}, "i am a heading");
+
+// jsx
+
+const headingJSX = <h1>I am a jsx heading</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// root.render(headingJSX)
+
+
+// react functional component
+
+const HeadingComponent1 = () => {
+    return <h1>Namaste React from functional component using return</h1>
+}
+
+const HeadingComponent2 = () => (
+    <div className="container">
+        <h1>Namaste React from functional component without using return</h1>
+        <HeadingComponent1/>
+    </div>
+)
+
+root.render(<HeadingComponent2/>)
